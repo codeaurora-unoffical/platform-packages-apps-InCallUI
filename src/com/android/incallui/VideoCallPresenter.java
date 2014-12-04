32c5595af76644e95a9f288091eb1cc89c5f5c79
Rekha Kumar <rekhak@codeaurora.org>
2014-12-03 17:41:57 -0800

    IMS-VT: Fix upgrade in call waiting scenarios
    -When one VoLTE call is active and another VoLTE call is on hold
     and upgrade button is hit, call ends. Fix this upgrade issue.
    
    Change-Id: Icab7ff30e7090be15d1d9c820f17ed774fc300f3
    CRs-Fixed: 753213

--- src/com/android/incallui/VideoCallPresenter.java
+++ src/com/android/incallui/VideoCallPresenter.java
@@ -369,6 +369,8 @@ public class VideoCallPresenter extends Presenter<VideoCallPresenter.VideoCallUi
             primary = callList.getIncomingCall();
         } else if (newState == InCallPresenter.InCallState.OUTGOING) {
             primary = callList.getOutgoingCall();
+        } else if (newState == InCallPresenter.InCallState.PENDING_OUTGOING) {
+            primary = callList.getPendingOutgoingCall();
         } else if (newState == InCallPresenter.InCallState.INCALL) {
             primary = callList.getActiveCall();
         }
@@ -381,14 +383,13 @@ public class VideoCallPresenter extends Presenter<VideoCallPresenter.VideoCallUi
             mPrimaryCall = primary;
 
             if (primary != null) {
-                checkForVideoCallChange();
-                checkForVideoStateChange();
+                updateVideoCall();
             } else if (primary == null) {
                 // If no primary call, ensure we exit video state and clean up the video surfaces.
                 exitVideoMode();
             }
         } else if(mPrimaryCall!=null) {
-            checkForVideoStateChange();
+            updateVideoCall();
         }
     }
 
@@ -448,8 +449,13 @@ public class VideoCallPresenter extends Presenter<VideoCallPresenter.VideoCallUi
             return;
         }
 
-        checkForVideoStateChange();
+        updateVideoCall();
         checkForCallSubstateChange();
+    }
+
+    private void updateVideoCall() {
+        checkForVideoCallChange();
+        checkForVideoStateChange();
     }
 
     /**
