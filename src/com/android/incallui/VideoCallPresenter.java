b73ad14755d8f90bd8e1e8c29af739bf5b43db9a
Nivedita Sarkar <nsarkar@codeaurora.org>
2015-01-15 11:25:40 -0800

    IMS-VT: Show glowpad view with accept/reject for most video upgrade cases
    - Show the glowpad view with all options only when upgrading
      from Volte to VT
    
    - For all other cases, show the glowpad with accept/reject only
    
    Change-Id: I41ecbda40db7c3c69428fc4272f8bfbd258e2980
    CRs-Fixed: 779208

--- src/com/android/incallui/VideoCallPresenter.java
+++ src/com/android/incallui/VideoCallPresenter.java
@@ -960,8 +960,8 @@ public class VideoCallPresenter extends Presenter<VideoCallPresenter.VideoCallUi
     }
 
     @Override
-    public void onUpgradeToVideoRequest(Call call) {
-        Log.d(this, "onUpgradeToVideoRequest call=" + call);
+    public void onUpgradeToVideoRequest(Call call, int videoState) {
+        Log.d(this, "onUpgradeToVideoRequest call = " + call + " new video state = " + videoState);
         if (mPrimaryCall == null || !Call.areSame(mPrimaryCall, call)) {
             Log.w(this, "UpgradeToVideoRequest received for non-primary call");
         }
@@ -970,8 +970,7 @@ public class VideoCallPresenter extends Presenter<VideoCallPresenter.VideoCallUi
             return;
         }
 
-        call.setSessionModificationState(
-                Call.SessionModificationState.RECEIVED_UPGRADE_TO_VIDEO_REQUEST);
+        call.setSessionModificationTo(videoState);
     }
 
     @Override
