360860b89ce0e955e39d4b9d4a415669b82dcc2a
Suresh Kumar Sugguna <sureshk@codeaurora.org>
2015-02-09 17:34:47 +0530

    IMS: Support IMS calls fallback to CS
    1. Display alert dialog to seek user confirmation.
    If confirmed for a retry, then initiate a cs call.
    2. In case of silent redial, automatically initiatte the cs call.
    
    Change-Id: I7847a9e604b1010bd2e2b4a4551d5e13e772c61e
    CRs-Fixed: 781748

--- src/com/android/incallui/InCallPresenter.java
+++ src/com/android/incallui/InCallPresenter.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ActivityNotFoundException;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.telecom.DisconnectCause;
 import android.telecom.PhoneAccount;
@@ -40,6 +41,7 @@ import android.os.PowerManager;
 
 import com.google.common.base.Preconditions;
 import com.android.incalluibind.ObjectFactory;
+import com.android.internal.telephony.TelephonyProperties;
 
 import java.util.Collections;
 import java.util.List;
@@ -850,6 +852,25 @@ public class InCallPresenter implements CallList.Listener, InCallPhoneListener {
     }
 
     /**
+     * Initiate a CS call.
+     */
+    public void dialCsCall(String number) {
+        Uri uri = Uri.fromParts("tel", number, null);
+        final Intent intent = new Intent(Intent.ACTION_CALL, uri);
+        intent.putExtra(TelephonyProperties.EXTRA_CALL_DOMAIN, true);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        try {
+            mContext.startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            // This is rather rare but possible.
+            // Note: this method is used even when the phone is encrypted. At
+            // that moment
+            // the system may not find any Activity which can accept this Intent
+            Log.e(this, "Activity for dialing new call isn't found.");
+        }
+    }
+
+    /**
      * For some disconnected causes, we show a dialog.  This calls into the activity to show
      * the dialog if appropriate for the call.
      */
@@ -859,7 +880,16 @@ public class InCallPresenter implements CallList.Listener, InCallPhoneListener {
             if (call.getAccountHandle() == null && !call.isConferenceCall()) {
                 setDisconnectCauseForMissingAccounts(call);
             }
-            mInCallActivity.maybeShowErrorDialogOnDisconnect(call);
+
+            int cause = call.getDisconnectCause().getCode();
+            if (cause == DisconnectCause.CALL_RETRY_BY_SILENT_REDIAL)
+            {
+                dialCsCall(call.getNumber());
+            } else if (cause == DisconnectCause.CALL_RETRY_BY_USER_CONSENT) {
+                mInCallActivity.showCsRedialDialogOnDisconnect(call.getNumber());
+            } else {
+                mInCallActivity.maybeShowErrorDialogOnDisconnect(call);
+            }
         }
     }
 
