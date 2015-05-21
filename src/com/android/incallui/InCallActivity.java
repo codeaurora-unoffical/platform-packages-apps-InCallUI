06512bca32c483bbfaa51cfe369e97497665be50
Nivedita Sarkar <nsarkar@codeaurora.org>
2015-05-21 16:13:14 -0700

    IMS-VT: Listen to sensor orientation changes and update camera preview
    - Register to listen to OrientationEventListener
    
    - Notify registrants of orientation changes and update camera
      preview size and orientation based on that.
    
    Change-Id: I6b62b5cc2457a43f07bf71a81f731d041a213576
    CRs-Fixed: 825825 837935

--- src/com/android/incallui/InCallActivity.java
+++ src/com/android/incallui/InCallActivity.java
@@ -52,6 +52,7 @@ import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.content.pm.ActivityInfo;
 
 import com.android.phone.common.animation.AnimUtils;
 import com.android.phone.common.animation.AnimationListenerAdapter;
@@ -92,6 +93,7 @@ public class InCallActivity extends Activity {
     private SuppServFailureNotificationReceiver mReceiver;
     private boolean mIsForegroundActivity;
     private AlertDialog mDialog;
+    private InCallOrientationEventListener mInCallOrientationEventListener;
 
     /** Use to pass 'showDialpad' from {@link #onNewIntent} to {@link #onResume} */
     private boolean mShowDialpadRequested;
@@ -129,11 +131,6 @@ public class InCallActivity extends Activity {
     private enum SuppService {
         UNKNOWN, SWITCH, SEPARATE, TRANSFER, CONFERENCE, REJECT, HANGUP;
     }
-
-    /**
-     * Used to determine if a change in orientation has occurred.
-     */
-    private static int mCurrentOrientation = Configuration.ORIENTATION_UNDEFINED;
 
     @Override
     protected void onCreate(Bundle icicle) {
@@ -204,6 +201,9 @@ public class InCallActivity extends Activity {
             mAnimateDialpadOnShow = false;
             mDtmfText = icicle.getString(DIALPAD_TEXT_EXTRA);
         }
+
+        mInCallOrientationEventListener = new InCallOrientationEventListener(this);
+
         if (isDsdaEnabled ) {
             initializeDsdaSwitchTab();
         }
@@ -233,10 +233,10 @@ public class InCallActivity extends Activity {
 
         // setting activity should be last thing in setup process
         InCallPresenter.getInstance().setActivity(this);
+        if (getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_SENSOR) {
+            enableInCallOrientationEventListener(true);
+        }
 
-        // It is possible that the activity restarted because orientation changed.
-        // Notify listeners if orientation changed.
-        doOrientationChanged(getResources().getConfiguration().orientation);
         InCallPresenter.getInstance().onActivityStarted();
     }
 
@@ -285,6 +285,7 @@ public class InCallActivity extends Activity {
     protected void onStop() {
         Log.d(this, "onStop()...");
 
+        enableInCallOrientationEventListener(false);
         InCallPresenter.getInstance().updateIsChangingConfigurations();
         InCallPresenter.getInstance().onActivityStopped();
         super.onStop();
@@ -493,22 +494,7 @@ public class InCallActivity extends Activity {
         InCallPresenter.getInstance().getProximitySensor().onConfigurationChanged(config);
         Log.d(this, "onConfigurationChanged "+config.orientation);
 
-        doOrientationChanged(config.orientation);
         super.onConfigurationChanged(config);
-    }
-
-
-    private void doOrientationChanged(int orientation) {
-        Log.d(this, "doOrientationChanged prevOrientation=" + mCurrentOrientation +
-                " newOrientation=" + orientation);
-        // Check to see if the orientation changed to prevent triggering orientation change events
-        // for other configuration changes.
-        if (orientation != mCurrentOrientation) {
-            mCurrentOrientation = orientation;
-            InCallPresenter.getInstance().onDeviceRotationChange(
-                    getWindowManager().getDefaultDisplay().getRotation());
-            InCallPresenter.getInstance().onDeviceOrientationChange(mCurrentOrientation);
-        }
     }
 
     public CallButtonFragment getCallButtonFragment() {
@@ -956,6 +942,19 @@ public class InCallActivity extends Activity {
             int phoneId = CallList.getInstance().getPhoneId(CallList
                     .getInstance().getActiveSubscription());
             bar.selectTab(bar.getTabAt(phoneId));
+        }
+    }
+
+    /**
+     * Enables the OrientationEventListener if enable flag is true. Disables it if enable is
+     * false
+     * @param enable true or false.
+     */
+    public void enableInCallOrientationEventListener(boolean enable) {
+        if (enable) {
+            mInCallOrientationEventListener.enable(enable);
+        } else {
+            mInCallOrientationEventListener.disable();
         }
     }
 
