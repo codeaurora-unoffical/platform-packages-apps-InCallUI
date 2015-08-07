b32dc9b39dd84a5d001291488d4080d0b13868c6
Muhammed Siju <msiju@codeaurora.org>
2015-08-07 18:00:40 +0530

    IMS-VT: Use correct view size when device is rotated.
    When peer_resolution received is portrait mode and the device
    is in landscape, the far-end video view will be re sized to
    fit the peer aspect ratio. If we rotate the device back to
    portrait, the view size needs to be re calculated based on
    the previously received peer_resolution
    
    Change-Id: I47b84fd823c6110dca87bcc372be1aaeb9888efa
    CRs-Fixed: 887207

--- src/com/android/incallui/VideoCallPresenter.java
+++ src/com/android/incallui/VideoCallPresenter.java
@@ -1099,6 +1099,12 @@ public class VideoCallPresenter extends Presenter<VideoCallPresenter.VideoCallUi
         Point size = ui.getScreenSize();
         Log.d("VideoCallPresenter", "setDisplayVideoSize: windowmgr width=" + size.x
                 + " windowmgr height=" + size.y);
+        size = resizeForAspectRatio(size, width, height);
+        ui.setDisplayVideoSize(size.x, size.y);
+    }
+
+    public static Point resizeForAspectRatio(Point inSize, int width, int height) {
+        Point size = new Point(inSize);
         if (size.y * width > size.x * height) {
             // current display height is too much. Correct it
             size.y = (int) (size.x * height / width);
@@ -1106,7 +1112,7 @@ public class VideoCallPresenter extends Presenter<VideoCallPresenter.VideoCallUi
             // current display width is too much. Correct it
             size.x = (int) (size.y * width / height);
         }
-        ui.setDisplayVideoSize(size.x, size.y);
+        return size;
     }
 
     private static boolean isAudioRouteEnabled(int audioRoute, int audioRouteMask) {
