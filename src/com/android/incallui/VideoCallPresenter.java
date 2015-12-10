15a9973ef5964c222d749fc0d05a1dceb01dddc5
Ravi Paluri <rpaluri@codeaurora.org>
2015-12-10 18:47:41 +0530

    IMS-VT: Play beep when uprade to video request is received.
    Play beep when uprade to video request is received
    
    Change-Id: Ib59a216e1b04b61b9852dd2825f2b1e6b169af4e
    CRs-Fixed: 950381

--- src/com/android/incallui/VideoCallPresenter.java
+++ src/com/android/incallui/VideoCallPresenter.java
@@ -1064,6 +1064,8 @@ public class VideoCallPresenter extends Presenter<VideoCallPresenter.VideoCallUi
         }
 
         mVideoCallHandler.removeMessages(EVENT_CLEAR_SESSION_MODIFY_REQUEST);
+
+        new VideoCallTonePlayer().playUpgradeToVideoRequestTone();
         call.setSessionModificationTo(videoState);
     }
 
