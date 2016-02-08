SlimRoms/packages_apps_InCallUI

IMS-VT: Animate dialpad on show when InCallActivity re-creates
If InCallActivity is relaunched from Dialer, always animate dialpad
on show. Not doing so is causing floating end button to overlap with
digits of dialpad.

Change-Id: Id2d63bba97f67c0f17b0cbc6696ef8fbd42c4e9c
CRs-Fixed: 953106

src/com/android/incallui/InCallActivity.java
@@ -220,7 +220,7 @@ public class InCallActivity extends Activity implements FragmentDisplayManager {
             if (icicle.containsKey(SHOW_DIALPAD_EXTRA)) {
                 boolean showDialpad = icicle.getBoolean(SHOW_DIALPAD_EXTRA);
                 mShowDialpadRequest = showDialpad ? DIALPAD_REQUEST_SHOW : DIALPAD_REQUEST_HIDE;
-                mAnimateDialpadOnShow = false;
+                mAnimateDialpadOnShow = showDialpad;
             }
             mDtmfText = icicle.getString(DIALPAD_TEXT_EXTRA);
 

codeaurora-unoffical/platform-packages-apps-InCallUI

src/com/android/incallui/InCallActivity.java
@@ -201,7 +201,7 @@ public class InCallActivity extends Activity {
             // in onResume() to ensure the hosting CallCardFragment has been inflated and is ready
             // to receive it.
             mShowDialpadRequested = icicle.getBoolean(SHOW_DIALPAD_EXTRA);
-            mAnimateDialpadOnShow = false;
+            mAnimateDialpadOnShow = mShowDialpadRequested;
             mDtmfText = icicle.getString(DIALPAD_TEXT_EXTRA);
         }
 